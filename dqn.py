b7f456b3bf296788265e286c0e8d84ec0e3d1dc7
faezs <faez.shakil@gmail.com>
2018-11-07 04:21:14 +0500

    implemented network skeleton construction

--- dqn.py
+++ dqn.py
@@ -2,60 +2,7 @@ from collections import namedtuple
 import gym
 import tensorflow as tf
 
-
-Transition = namedtuple('Transition', ['phi_t',
-                                       'a_t',
-                                       'r_t',
-                                       'phi_next'])
-
-class DQNAgent(object):
-    def __init__(self, env, n_episodes, replay_capacity, arch=None, action_space=None):
-        self.env = env
-        self.n_episodes = n_episodes
-        self.replay_capacity = replay_capacity
-        self.arch = arch or 'deepmind_cnn'
-        self.history = []
-
-    def init_replay_memory(self):
-        self.replay_memory = []
-
-    def phi(self, state):
-        """
-        Expects
-        """
-        return state
-
-    def construct_Q(self, graph=None):
-        if not graph:
-            graph = tf.Graph()
-        with graph.as_default() as g:
-            self.construct_Q()
-        return
-
-    def eps_greedy_Q(self, x_t):
-        q_outputs = self.Q.predict(x_t)
-
-
-    def run_episode(self):
-        self.env.reset()
-        obs, _, _, _ = self.env.step(self.env.action_space.sample())
-        st = (obs, )
-        phi = self.phi(st)
-        while True:
-            action = self.eps_greedy_Q(obs)
-            obs_next, r, done, info = self.env.step(action)
-            if done:
-                break
-            st_next = (st, action, obs_next)
-            phi_next = self.phi(st_next)
-            transition = (phi, action, r, phi_next)
-            self.save_transition(transition)
-
-            self.run_training_step(self.sample_transitions())
-            obs = obs_next
-            st = st_next
-            phi = phi_next
-        return
+tf.logging.set_verbosity(tf.logging.ERROR)
 
 def phi(images,
         resize_to=(110, 84),
@@ -94,3 +41,144 @@ def phi(images,
                                                  target_width=crop_to[1])
 
         return square
+
+
+def conv2d(inputs, name,
+           n_filters, kernel_size, strides,
+           activation=tf.nn.relu, reuse=False):
+    return tf.layers.conv2d(inputs,
+                            filters=n_filters,
+                            kernel_size=kernel_size,
+                            strides=strides,
+                            padding='valid',
+                            activation=activation,
+                            name=name,
+                            reuse=reuse)
+
+def fc(inputs, name, units, activation=None, reuse=False):
+    return tf.layers.dense(inputs,
+                           units=units,
+                           activation=activation,
+                           name=name,
+                           reuse=reuse)
+
+def flatten(inputs, name):
+    return tf.layers.flatten(inputs, name=name)
+
+# To use these with the functions in this lib,
+# add the input tensor and a reuse flag
+# in the construction context
+
+Conv2DSpec = namedtuple('Conv2DSpec', ['name',
+                                       'n_filters',
+                                       'kernel_size',
+                                       'strides',
+                                       'activation'])
+
+FCSpec = namedtuple('FCSpec', ['name',
+                               'units',
+                               'activation'])
+
+FlattenSpec = namedtuple('FlattenSpec', ['name'])
+
+SPEC_TO_LAYER = {Conv2DSpec: conv2d,
+                 FCSpec: fc,
+                 FlattenSpec: flatten}
+
+def make_layer(inp, spec, reuse=False):
+    ltype = type(spec)
+    if not ltype in SPEC_TO_LAYER:
+        raise TypeError('{} not found in layer library: {}'.format(ltype, SPEC_TO_LAYER.keys()))
+    l_fn = SPEC_TO_LAYER[ltype]
+    l_args = spec._asdict()
+    l_args['inputs'] = inp
+    if reuse:
+        l_args['reuse'] = reuse
+    return l_fn(**l_args)
+
+deepmind_arch = (Conv2DSpec(name='conv1',
+                            n_filters=16,
+                            kernel_size=(16, 16),
+                            strides=(4, 4),
+                            activation=tf.nn.relu),
+                 Conv2DSpec(name='conv2',
+                            n_filters=32,
+                            kernel_size=(4, 4),
+                            strides=(2, 2),
+                            activation=tf.nn.relu),
+                 FlattenSpec(name='flatten_conv2'),
+                 FCSpec(name='fc1',
+                        units=256,
+                        activation=tf.nn.relu))
+
+Transition = namedtuple('Transition', ['phi_t',
+                                       'a_t',
+                                       'r_t',
+                                       'phi_next'])
+
+class DQNAgent(object):
+    def __init__(self, env, n_episodes,
+                 replay_capacity,
+                 action_dim,
+                 arch=deepmind_arch):
+        self.env = env
+        self.n_episodes = n_episodes
+        self.replay_capacity = replay_capacity
+        self.arch = arch
+        self.action_dim = action_dim
+        self.history = []
+
+    def _init_replay_memory(self):
+        self.replay_memory = []
+
+    def _phi(self, state):
+        """
+        Expects
+        """
+        return state
+
+    def _output_layer(self, inp, reuse=False):
+        s = FCSpec(name='output',
+                   units=self.action_dim,
+                   activation=None)
+        return make_layer(inp, s, reuse=reuse)
+
+    def _construct_Q(self, inputs, name, reuse=False):
+        x = inputs
+        with tf.name_scope(name):
+            for spec in self.arch:
+                x = make_layer(x, spec, reuse)
+            output = self._output_layer(x, reuse=reuse)
+        return output
+
+    def _dq_loss(self):
+        return
+
+
+    def _training_op(self):
+        return
+
+    def _eps_greedy_Q(self, x_t):
+        q_outputs = self.Q.predict(x_t)
+
+
+    def run_episode(self):
+        self.env.reset()
+        obs, _, _, _ = self.env.step(self.env.action_space.sample())
+        st = (obs, )
+        phi = self.phi(st)
+        while True:
+            action = self.eps_greedy_Q(obs)
+            obs_next, r, done, info = self.env.step(action)
+            if done:
+                break
+            st_next = (st, action, obs_next)
+            phi_next = self.phi(st_next)
+            transition = (phi, action, r, phi_next)
+            self.save_transition(transition)
+
+            self.run_training_step(self.sample_transitions())
+            obs = obs_next
+            st = st_next
+            phi = phi_next
+        return
--- test_dqn.py
+++ test_dqn.py
@@ -1,19 +1,53 @@
-from dqn import phi
+from dqn import phi, DQNAgent
 import unittest
 import numpy as np
 import tensorflow as tf
-import logging
 
+tf.logging.set_verbosity(tf.logging.ERROR)
 
 class DQNTest(tf.test.TestCase):
+
+    def tearDown(self):
+        tf.reset_default_graph()
+
+    @staticmethod
+    def get_rand(size, tensor=False):
+        r = np.random.uniform(low=0.0, high=255.0,
+                              size=size)
+        if not tensor:
+            return r
+        return tf.convert_to_tensor(r, dtype=tf.float32)
+
+    @staticmethod
+    def get_agent(env):
+       action_dim = 4
+       replay_capacity = 1e6
+       n_episodes = 10e7
+
+       return DQNAgent(env=env,
+                       n_episodes=n_episodes,
+                       replay_capacity=replay_capacity,
+                       action_dim=action_dim)
+
     def testPhi(self):
-        tf.logging.set_verbosity(logging.WARN)
         b = 64
         input_shape = (b, 4, 210, 160, 3)
         target_shape = (b, 84, 84, 4)
-        images = np.random.uniform(low=0.0, high=255.0,
-                                   size=input_shape)
-        with self.test_session():
+        images = self.get_rand(input_shape)
+        with self.cached_session():
             small_squares = phi(images)
             actual_shape = small_squares.eval().shape
             self.assertEqual(actual_shape, target_shape)
+
+    def testQNetworkShape(self):
+        env = None
+        b_size = 32
+        agent = self.get_agent(env)
+
+        # A Q-network maps input_shape -> [b_size, action_dim]
+        input_shape = [b_size, 84, 84, 4]
+        target_shape = [b_size, agent.action_dim]
+        sample_state = self.get_rand(input_shape, tensor=True)
+        outputs = agent._construct_Q(sample_state, name='test', reuse=False)
+        self.assertEqual(outputs.get_shape().as_list(),
+                         target_shape)
