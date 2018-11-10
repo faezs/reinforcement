da27e7ba4d26d96882dfe2a47268a9fa4bd23695
faezs <faez.shakil@gmail.com>
2018-11-11 03:27:33 +0500

    CartPole Solved!!!

--- dqn.py
+++ dqn.py
@@ -1,8 +1,56 @@
 from collections import namedtuple
 import gym
 import tensorflow as tf
+import numpy as np
+from utils import combined_shape, placeholder, placeholder_from_space
 
-tf.logging.set_verbosity(tf.logging.ERROR)
+
+Transition = namedtuple('Transition', ['obs_t',
+                                       'act_t',
+                                       'r_t',
+                                       'obs_tn',
+                                       'done'])
+
+ActFeed = namedtuple('ActFeed', ['phi_t',
+                                 'stochastic',
+                                 'update_eps'])
+
+
+class ReplayMemory(object):
+    def __init__(self, obs_dim, act_dim, r_dim, size):
+        self.obs_buf = np.zeros(shape=combined_shape(size, obs_dim))
+        self.act_buf = np.zeros(shape=combined_shape(size, act_dim))
+        self.r_buf = np.zeros(shape=combined_shape(size, r_dim))
+        self.obs_tn_buf = np.zeros(shape=combined_shape(size, obs_dim))
+        self.done_buf = np.zeros(shape=combined_shape(size), dtype=np.float32)
+        self.idx = 0
+        self.max_idx = size
+
+    def store(self, obs_t, act_t, r_t, obs_tn, done):
+        if not self.idx < self.max_idx:
+            raise Exception('Memory Capacity Exceeded')
+        self.obs_buf[self.idx] = obs_t
+        self.act_buf[self.idx] = act_t
+        self.r_buf[self.idx] = r_t
+        self.obs_tn_buf[self.idx] = obs_tn
+        self.done_buf[self.idx] = done
+        self.idx += 1
+
+    def sample(self, batch_size=32):
+        sample_idxs = np.random.choice(self.idx,
+                                       batch_size,
+                                       replace=True)
+        obs_mb =self.obs_buf[sample_idxs, :]
+        act_mb =self.act_buf[sample_idxs, :]
+        r_mb = self.r_buf[sample_idxs, :]
+        obs_tn_mb = self.obs_tn_buf[sample_idxs, :]
+        done_mb = self.done_buf[sample_idxs]
+        return Transition(obs_t=obs_mb,
+                          act_t=np.squeeze(act_mb),
+                          r_t=np.squeeze(r_mb),
+                          obs_tn=obs_tn_mb,
+                          done=np.squeeze(done_mb))
+
 
 def phi(images,
         resize_to=(110, 84),
@@ -111,74 +159,230 @@ deepmind_arch = (Conv2DSpec(name='conv1',
                         units=256,
                         activation=tf.nn.relu))
 
-Transition = namedtuple('Transition', ['phi_t',
-                                       'a_t',
-                                       'r_t',
-                                       'phi_next'])
+dnn_arch = (FCSpec(name='fc1',
+                   units=256,
+                   activation=tf.nn.relu),
+            FCSpec(name='fc2',
+                   units=256,
+                   activation=tf.nn.relu),
+            FCSpec(name='fc3',
+                   units=256,
+                   activation=tf.nn.relu))
+
+
+archs = {'dnn':dnn_arch,
+         'deepmind':deepmind_arch}
+
 
 class DQNAgent(object):
-    def __init__(self, env, n_episodes,
+    def __init__(self,
+                 env,
+                 n_episodes,
                  replay_capacity,
-                 action_dim,
-                 arch=deepmind_arch):
+                 arch='dnn'):
+        if not arch in archs:
+            raise TypeError('{} not in registered architectures'.format(arch))
         self.env = env
         self.n_episodes = n_episodes
-        self.replay_capacity = replay_capacity
-        self.arch = arch
-        self.action_dim = action_dim
+        self.replay_capacity = int(replay_capacity)
+        self.arch = archs[arch]
         self.history = []
+        self.batch_size = 32
+        self.num_actions = int(self.env.action_space.n)
 
     def _init_replay_memory(self):
-        self.replay_memory = []
-
-    def _phi(self, state):
-        """
-        Expects
-        """
-        return state
+        self._buffer = ReplayMemory(self.env.observation_space.shape, 1, 1, size=self.replay_capacity)
 
     def _output_layer(self, inp, reuse=False):
         s = FCSpec(name='output',
-                   units=self.action_dim,
+                   units=self.env.action_space.n,
                    activation=None)
         return make_layer(inp, s, reuse=reuse)
 
-    def _construct_Q(self, inputs, name, reuse=False):
+    def _theta(self, inputs, name, reuse=False):
         x = inputs
-        with tf.name_scope(name):
+        with tf.variable_scope(name):
             for spec in self.arch:
                 x = make_layer(x, spec, reuse)
             output = self._output_layer(x, reuse=reuse)
         return output
 
-    def _dq_loss(self):
-        return
+    def _transition_phs(self):
+        return Transition(
+            obs_t=placeholder_from_space(self.env.observation_space),
+            act_t=tf.placeholder(shape=[None], dtype=tf.int32),
+            r_t=placeholder(dim=None),
+            obs_tn=placeholder_from_space(self.env.observation_space),
+            done=placeholder(None))
+
+    def _act_phs(self):
+        return ActFeed(
+            phi_t=placeholder_from_space(self.env.observation_space),
+            stochastic=tf.placeholder(tf.bool, (), name='stochastic'),
+            update_eps=tf.placeholder(tf.float32, (), name='update_eps'))
+
+    def _make_feed(self, phs, vals):
+        # phs and vals must be the same kind of namedtuple
+        assert type(phs) == type(vals), (phs, vals)
+        # phs: {str, tf.placeholders}
+        # vals: {str, np.array}
+        feeds = {}
+        for ph, val in zip(phs, vals):
+            feeds[ph] = val
+        return feeds
+
+    def _act(self):
+        phi_t, stochastic_ph, update_eps_ph = phs = self._act_phs()
+
+        eps = tf.get_variable("eps", (), initializer=tf.constant_initializer(0))
+
+        q_t = self._theta(phi_t, 'theta_t')
+        deterministic_actions = tf.argmax(q_t, axis=1)
+
+        batch_size = tf.shape(phi_t)[0]
+
+        random_actions = tf.random_uniform(tf.stack([batch_size]), minval=0, maxval=self.num_actions, dtype=tf.int64)
+
+        choose_random = tf.random_uniform(tf.stack([batch_size]), minval=0, maxval=1, dtype=tf.float32) < eps
+
+        stochastic_actions = tf.where(choose_random, random_actions, deterministic_actions)
+
+        eps_greedy_actions = tf.cond(stochastic_ph, lambda: stochastic_actions, lambda:deterministic_actions)
+
+        update_eps_op = eps.assign(tf.cond(update_eps_ph >= 0, lambda: update_eps_ph, lambda: eps))
+
+        return phs, eps_greedy_actions, update_eps_op
 
 
-    def _training_op(self):
-        return
+    def _train_and_update_ops(self, loss, theta_t, theta_tn):
+        opt = tf.train.RMSPropOptimizer(learning_rate=0.001)
+        train = opt.minimize(loss, var_list=theta_t)
 
-    def _eps_greedy_Q(self, x_t):
-        q_outputs = self.Q.predict(x_t)
+        update_target_ops = []
+        for var, var_target in zip(sorted(theta_t, key=lambda v: v.name),
+                                   sorted(theta_tn, key=lambda v: v.name)):
+            update_target_ops.append(var_target.assign(var))
+
+        update_target_op = tf.group(*update_target_ops)
+        return train, update_target_op
+
+    def _build_deepQ_graph(self, gamma=1.0):
+
+        # Add Action Ops
+        act_feeds, eps_greedy_actions, eps_update = self._act()
+
+        # Add Transition Placeholders
+        phi_t, a_t, r_t, phi_tn, done = phs = self._transition_phs()
+
+        # Add action value function for transition initial state (reusing self._act weights)
+        q_t = self._theta(phi_t, 'theta_t', reuse=True)
+        q_t_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='theta_t')
+
+        # Add action value function for transition terminal state
+        # (no weight reuse since the target network doesn't exist in the _act graph)
+        q_tn = self._theta(phi_tn, 'theta_tn')
+        q_tn_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='theta_tn')
+
+        # Scores for initial selected actions in these transitions
+        y_i = tf.reduce_sum(q_t * tf.one_hot(a_t, self.num_actions), 1)
+
+        # max_a_Q* i.e RHS of Bellman's Equation
+        q_tn_best = tf.reduce_max(q_tn, 1)
+        q_tn_best_masked = (1.0 - done) * q_tn_best
+        q_tn_action_reward = r_t + gamma * q_tn_best_masked
+
+        # Loss function derived from Bellman's Equation
+        loss = tf.square(y_i - q_tn_action_reward)
+
+        # SGD op on Loss function applied to q_function_act
+        # Update op for setting q_function_train weights to q_function_act weights
+        # (which are the ones actually optimized by SGD)
+        train_qn, q_tn_update_op = self._train_and_update_ops(loss, q_t_vars, q_tn_vars)
+
+        return act_feeds, eps_greedy_actions, eps_update, phs, train_qn, q_tn_update_op
+
+    def k_skip_sample(self, a):
+        samples = zip(*(self.env.sample(a) for _ in range(self.skip_k)))
+        obs_tn, r_t, dones, info = samples
+        #return np.stack(obs_tn, axis=0), r_t = np.st
 
 
-    def run_episode(self):
+    def learn(self, n_episodes=100000, start_training=32, batch_size=32, update_qtn_every=64):
+        self._init_replay_memory()
+
         self.env.reset()
-        obs, _, _, _ = self.env.step(self.env.action_space.sample())
-        st = (obs, )
-        phi = self.phi(st)
-        while True:
-            action = self.eps_greedy_Q(obs)
-            obs_next, r, done, info = self.env.step(action)
-            if done:
-                break
-            st_next = (st, action, obs_next)
-            phi_next = self.phi(st_next)
-            transition = (phi, action, r, phi_next)
-            self.save_transition(transition)
 
-            self.run_training_step(self.sample_transitions())
-            obs = obs_next
-            st = st_next
-            phi = phi_next
-        return
+        # initialize eps decay generator
+        eps_val = linear_decay(init=1, final=0.1, n_steps=1000)
+
+        with tf.Session() as session:
+            # Build e-greedy policy and q-networks training graph
+            act_phs, eps_greedy_actions, eps_update_op, transition_phs, train_qn_op, update_qtn_op = self._build_deepQ_graph()
+
+            # Initialize tf variables
+            # set qtn weights to be equal to qt weights
+            session.run(tf.global_variables_initializer())
+            session.run(update_qtn_op)
+
+            eps_reward = []
+            for i in range(n_episodes):
+                self.env.reset()
+                done = False
+                stochastic = True
+                obs_t  = self.env.reset()
+                t = 0
+                eps_reward.append(0.0)
+                while not done:
+                    # 1) Get Action for current observation
+                    a_t, _ = session.run(fetches=[eps_greedy_actions, eps_update_op],
+                                      feed_dict=self._make_feed(act_phs, ActFeed(phi_t=np.array(obs_t)[None],
+                                                                                 stochastic=stochastic,
+                                                                                 update_eps=next(eps_val))))
+
+                    # From Array to Scalar
+                    a_t = a_t[0]
+                    obs_tn, r_t, done, info = self.env.step(a_t)
+
+                    eps_reward[i] += r_t
+                    #print('episode: {}, timestep: {}, action: {}, cumulative_reward: {}'.format(i, t, a_t, eps_reward[i]))
+                    self.env.render()
+
+                    self._buffer.store(obs_t=obs_t, act_t=a_t, r_t=r_t, obs_tn=obs_tn, done=float(done))
+
+                    if t > start_training:
+                        _ = session.run(fetches=[train_qn_op],
+                                        feed_dict=self._make_feed(transition_phs,
+                                                                  self._buffer.sample(batch_size)))
+                    if t % update_qtn_every:
+                        _ = session.run(fetches=[train_qn_op, update_qtn_op],
+                                        feed_dict=self._make_feed(transition_phs,
+                                                                  self._buffer.sample(batch_size)))
+
+                    obs_t = obs_tn
+                    t += 1
+                print('episode: {}, timesteps: {}, cumulative_reward: {}'.format(i, t, eps_reward[i]))
+
+
+def linear_decay(init, final, n_steps):
+    delta = (init - final) / n_steps
+    assert init > final, 'init: {} must be greater than final: {}'.format(init, final)
+    counter = init
+    for i in range(n_steps):
+        yield counter
+        counter = counter - delta
+    while True:
+        yield counter
+
+def get_agent(env, **kwargs):
+    replay_capacity = 1e6
+    n_episodes = 10e7
+
+    return DQNAgent(env=env or gym.make('CartPole-v0'),
+                    n_episodes=n_episodes,
+                    replay_capacity=replay_capacity,
+                    **kwargs)
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v0')
+    agent = get_agent(env)
+    agent.learn()
--- test_dqn.py
+++ test_dqn.py
@@ -1,7 +1,10 @@
-from dqn import phi, DQNAgent
+from dqn import phi, DQNAgent, ReplayMemory, linear_decay
 import unittest
 import numpy as np
 import tensorflow as tf
+from utils import combined_shape
+import gym
+
 
 tf.logging.set_verbosity(tf.logging.ERROR)
 
@@ -19,15 +22,14 @@ class DQNTest(tf.test.TestCase):
         return tf.convert_to_tensor(r, dtype=tf.float32)
 
     @staticmethod
-    def get_agent(env):
-       action_dim = 4
+    def get_agent(env, **kwargs):
        replay_capacity = 1e6
        n_episodes = 10e7
 
-       return DQNAgent(env=env,
+       return DQNAgent(env=env or gym.make('CartPole-v0'),
                        n_episodes=n_episodes,
                        replay_capacity=replay_capacity,
-                       action_dim=action_dim)
+                       **kwargs)
 
     def testPhi(self):
         b = 64
@@ -39,15 +41,53 @@ class DQNTest(tf.test.TestCase):
             actual_shape = small_squares.eval().shape
             self.assertEqual(actual_shape, target_shape)
 
-    def testQNetworkShape(self):
-        env = None
+    def testDNNThetaShape(self):
         b_size = 32
-        agent = self.get_agent(env)
+        agent = self.get_agent(None, arch='deepmind')
+        out_dim = int(agent.env.action_space.n)
 
         # A Q-network maps input_shape -> [b_size, action_dim]
         input_shape = [b_size, 84, 84, 4]
-        target_shape = [b_size, agent.action_dim]
+        target_shape = [b_size, out_dim]
         sample_state = self.get_rand(input_shape, tensor=True)
-        outputs = agent._construct_Q(sample_state, name='test', reuse=False)
+        outputs = agent._theta(sample_state, name='test', reuse=False)
         self.assertEqual(outputs.get_shape().as_list(),
                          target_shape)
+
+    def testReplayMemory(self):
+        od = [84, 84, 4]
+        ad = [8, 10]
+        rd = [5]
+        s = int(10000)
+        b = 32
+
+        rm = ReplayMemory(obs_dim=od,
+                          act_dim=ad,
+                          r_dim=rd,
+                          size=s)
+        o = self.get_rand(od)
+        a = self.get_rand(ad)
+        r = self.get_rand(rd)
+        d = 0
+        for _ in range(1000):
+            rm.store(o, a, r, o, d)
+
+        o_s, a_s, r_s, on_s, d_s = rm.sample(b)
+
+        self.assertEqual(o_s.shape, combined_shape(b, od))
+        self.assertEqual(a_s.shape, combined_shape(b, ad))
+        self.assertEqual(r_s.shape, combined_shape(b, rd))
+        self.assertEqual(on_s.shape, combined_shape(b, od))
+        self.assertEqual(d_s.shape, combined_shape(b))
+
+    def testE2E_Q(self):
+        env = gym.make('CartPole-v0')
+        agent = self.get_agent(env)
+        #agent.learn()
+
+    def test_linear_schedule(self):
+        lsched = linear_decay(init=1.0, final=0.0, n_steps=10)
+        init = 1
+        for i in range(11):
+            s = next(lsched)
+        self.assertAlmostEqual(s, 0)
